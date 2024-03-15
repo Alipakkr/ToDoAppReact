bb01442e1f5552e2e5eb246de12ac27b0222cf29
Alipa <alipakkr24@gmail.com>
2024-03-15 07:52:23 +0530

    todoapp

---
+++ backend/Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const routes = require("./routes/ToDoRoutes");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
